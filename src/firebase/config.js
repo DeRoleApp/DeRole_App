d1eb59c3a5e73e9527f46dfe0972f16a0f694776
rafael <rcborges98@gmail.com>
2024-04-16 00:53:04 -0300

    chore(firebase): move firebase folder into src folder and add firestore initialization

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAwfAvz7KMNiZK7DagSBIUF6-ACPiyzASI",
@@ -10,3 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const firestore = () => getFirestore(app);
