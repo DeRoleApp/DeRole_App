c6baf8cfdaa3237dfcdb0cfc06afa7ad4aa0e3de
rafael <rcborges98@gmail.com>
2024-04-15 23:29:23 -0300

    feat: add firebase

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwfAvz7KMNiZK7DagSBIUF6-ACPiyzASI",
+  authDomain: "de-role-63ac9.firebaseapp.com",
+  projectId: "de-role-63ac9",
+  storageBucket: "de-role-63ac9.appspot.com",
+  messagingSenderId: "241791855463",
+  appId: "1:241791855463:web:d713b46460a936e87e5dc2"
+};
+
+const app = initializeApp(firebaseConfig);
